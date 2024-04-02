0fe50718c67ca70f18ee8531dca02224b2a21f24
Starrleto <ellierasuli@gmail.com>
2024-04-01 21:52:45 -0700

    Calc

---
+++ src/scripts/script.ts
@@ -0,0 +1,7 @@
+const calculateTip = (amount: number, percent: number) => {
+    return (amount * percent);
+}
+
+const perPerson = (people: number, amount: number) => {
+    return (amount / people);
+}
